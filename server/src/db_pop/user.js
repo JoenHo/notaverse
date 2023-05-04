1ae5ba7a9303210e03694a9e1209f95356f079c0
Youjin6 <70083559+Youjin6@users.noreply.github.com>
2023-05-04 12:32:38 -0700

    Update JSON data population in user.js

--- server/src/db_pop/user.js
+++ server/src/db_pop/user.js
@@ -1,37 +1,41 @@
 db = db.getSiblingDB('notaverse');
+// drop current collections
+db.users.drop();
+db.notes.drop();
+
+// create user, notes collection
 db.createCollection("users")
+db.createCollection("notes")
+
+// get userCollection, noteCollection
 userCollection = db.getCollection("users");
 noteCollection = db.getCollection("notes");
 
+// JSON data for user
 data = [
     {
         username: 'adam_anderson',
         email: 'adom.a@example.com',
-        password: 'J@ARGgre!2023',
         plan: 'Premium'
     },
     {
         username: 'john_smith1',
         email: 'john.smith@example.com',
-        password: 'J0hn$eh#2023',
         plan: 'Free'
     },
     {
         username: 'michael_jackson1',
         email: 'michael.jackson@example.com',
-        password: 'M1ch@elargaeJ@cks0n',
         plan: 'Premium'
         },
     {
         username: 'emma_watson1',
         email: 'emma.watson@example.com',
-        password: 'wae@W@ts0n!2023',
         plan: 'Free'
         },
     {
         username: 'david_johnson1',
         email: 'david.johnson@example.com',
-        password: 'D@gwefe$0n',
         plan: 'Premium'
         },
     {
@@ -41,25 +45,38 @@ data = [
         plan: 'Premium'
         },
 ];
-    
+
+// insert data to userCollection
 userCollection.insertMany(data);
 
 
-let user = userCollection.findOne({username:'adam_anderson'});
-let user_id = user._id;
-noteData = [
-  {
-    "user": user_id,
-    "title": "Note 100",
-    "img_url": "https://example.com/forest.jpg",
-    "content": "A peaceful walk through the forest offers a chance to reconnect with nature."
-  },
-  {
-    "user": user_id,
-    "title": "Note 200",
-    "img_url": "https://example.com/forest.jpg",
-    "content": "A peaceful walk through the forest offers a chance to reconnect with nature."
-  }
-] 
+// Create notes and add them to Adam
+let adam = userCollection.findOne({username:'adam_anderson'});
+let user_id = adam._id;
 
-noteCollection.insertMany(noteData);
+// Json data for note
+noteData = 
+  {
+    "user_id": user_id,
+    "title": "A Cozy Reading Nook",
+    "img_url": "https://example.com/readingnook.jpg",
+    "content": "A comfortable chair, warm blanket, and a good book create the ideal reading nook."
+  }
+
+// Insert to noteCollection
+noteCollection.insertOne(noteData);
+
+// Retrieve the notes that match the provided user ID.
+let note = noteCollection.findOne({user_id:user_id});
+
+// Create a notes field for the user
+db.users.updateOne(
+  { "_id": user_id },
+  { "$set": { "notes": [] } },
+);
+
+// Add the id of note to notes field of Adam
+db.users.updateOne(
+  { "_id": user_id},
+  {"$push": { "notes": note._id} }
+);
