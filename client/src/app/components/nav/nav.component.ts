c736885e2e65923db4f7bf95c76cdf8bab95c5ce
J.H <59863142+JoenHo@users.noreply.github.com>
2023-05-14 16:30:11 -0700

    Update nav component

--- client/src/app/components/nav/nav.component.ts
+++ client/src/app/components/nav/nav.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss']
 })
-export class NavComponent {
+export class NavComponent implements OnInit {
+  @Input() id: string = ''; // userID
+  
+  user: any;
 
+  /** Constructor */
+  constructor(private userService: UserService) {}
+
+  /** OnInit hook */
+  ngOnInit(): void {
+    this.fetchUser()
+  }
+
+  /** Fetch user data */
+  fetchUser() {
+    return this.userService.getUserById(this.id).subscribe((data: any) => {
+      this.user = data;
+    });        
+  }
 }
