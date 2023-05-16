d30ad17a82691ec87f71e5c1ec23d6fd82771f3c
J.H <59863142+JoenHo@users.noreply.github.com>
2023-05-16 15:30:20 -0700

    Update room component to show active room info

--- client/src/app/components/room/room.component.ts
+++ client/src/app/components/room/room.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnChanges, SimpleChanges } from '@angular/core';
+import { RoomService } from '../../services/room.service';
+import { RoomItemService } from '../../services/room-item.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-room',
   templateUrl: './room.component.html',
   styleUrls: ['./room.component.scss']
 })
-export class RoomComponent {
+export class RoomComponent implements OnInit {
+    
+  room: any = Observable<any>;
+  roomItems: any = [];
+
+  /** Constructor */
+  constructor(
+    private roomService: RoomService,
+    private roomItemService: RoomItemService,
+  ) { }
+
+  ngOnInit(): void {
+    this.roomService.active_room.subscribe((room: any) => {
+      this.room = room;
+    });
+  }
 
 }
