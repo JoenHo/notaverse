5278032ba4efebc0b9c547b60642d1fe569e79bc
Weizheng Zhang <vvqqdd@gmail.com>
2023-05-28 18:39:10 -0700

    Added mongo db atlas connection

--- server/src/db.ts
+++ server/src/db.ts
@@ -1,22 +1,25 @@
 import mongoose from 'mongoose';
-import {config} from './config';
+import { config } from './config';
 
 class DataAccess {
-    static mongooseInstance: any;
-    static mongooseConnection: mongoose.Connection;
+  static mongooseInstance: any;
+  static mongooseConnection: mongoose.Connection;
 
-    /** Constructor for DataAccess class */
-    constructor () {
-        DataAccess.connect();
-    }
-    
-    /** Connect to MongoDB though mongoose */
-    static connect (): Promise<mongoose.Connection> {
-        if(this.mongooseInstance) return Promise.resolve(this.mongooseInstance);
-        this.mongooseConnection  = mongoose.connection;
-        this.mongooseInstance = mongoose.connect(config.mongo.url);
-        return this.mongooseInstance;
-    }
+  static DB_CONNECTION_STRING: string =
+    'mongodb+srv://notaverse_admin:5SORp7DVWRt9VDZv@cluster0.wxbgpj2.mongodb.net/?retryWrites=true&w=majority';
+
+  /** Constructor for DataAccess class */
+  constructor() {
+    DataAccess.connect();
+  }
+
+  /** Connect to MongoDB though mongoose */
+  static connect(): Promise<mongoose.Connection> {
+    if (this.mongooseInstance) return Promise.resolve(this.mongooseInstance);
+    this.mongooseConnection = mongoose.connection;
+    this.mongooseInstance = mongoose.connect(config.mongo.url);
+    return this.mongooseInstance;
+  }
 }
 DataAccess.connect();
-export {DataAccess};
+export { DataAccess };
