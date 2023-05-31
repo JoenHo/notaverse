8cdb11c05b054bdd699e759c1ecc8280b0298c24
J.H <59863142+JoenHo@users.noreply.github.com>
2023-05-31 16:29:51 -0700

    Update DataAccess class and move MongoDB Atlas URI to config

--- server/src/config.ts
+++ server/src/config.ts
@@ -1,11 +1,21 @@
 const SERVER_PORT = 3000;
 const MONGO_URL = 'mongodb://127.0.0.1:27017/notaverse';
+const ATLAS_URL = 'mongodb+srv://notaverse_admin:5SORp7DVWRt9VDZv@cluster0.wxbgpj2.mongodb.net/?retryWrites=true&w=majority';
+const GOOGLE_CLIENT_ID = '241156039018-672vr2ogqejvlcb2um2em0md49uhh6fm.apps.googleusercontent.com';
+const GOOGLE_SECRET_ID = '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';
 
 export const config = {
     mongo: {
         url: MONGO_URL
     },
+    mongo_atlas: {
+        url: ATLAS_URL
+    },
     server: {
         port: SERVER_PORT
+    },
+    google_auth: {
+        client_id: GOOGLE_CLIENT_ID,
+        secret_id: GOOGLE_SECRET_ID
     }
 };
--- server/src/db.ts
+++ server/src/db.ts
@@ -1,30 +1,23 @@
-import Mongoose = require('mongoose');
-import { config } from './config';
+import mongoose from 'mongoose';
+import {config} from './config';
 
 class DataAccess {
-  static mongooseInstance: any;
-  static mongooseConnection: Mongoose.Connection;
+    static mongooseInstance: any;
+    static mongooseConnection: mongoose.Connection;
 
-  static DB_CONNECTION_STRING: string =
-    'mongodb+srv://notaverse_admin:5SORp7DVWRt9VDZv@cluster0.wxbgpj2.mongodb.net/?retryWrites=true&w=majority';
+    /** Constructor for DataAccess class */
+    constructor () {
+        DataAccess.connect();
+    }
 
-  /** Constructor for DataAccess class */
-  constructor() {
-    DataAccess.connect();
-  }
-
-  /** Connect to MongoDB though mongoose */
-  static connect(): Mongoose.Connection {
-    if (this.mongooseInstance) return this.mongooseInstance;
-
-    this.mongooseConnection = Mongoose.connection;
-    this.mongooseConnection.on('open', () => {
-      console.log('Connected to mongodb.');
-    });
-
-    this.mongooseInstance = Mongoose.connect(this.DB_CONNECTION_STRING);
-    return this.mongooseInstance;
-  }
+    /** Connect to MongoDB though mongoose */
+    static connect (): Promise<mongoose.Connection> {
+        if(this.mongooseInstance) return Promise.resolve(this.mongooseInstance);
+        this.mongooseConnection  = mongoose.connection;
+        this.mongooseInstance = mongoose.connect(config.mongo.url); // Local URL
+        // this.mongooseInstance = mongoose.connect(config.mongo_atlas.url); // Atlas URL
+        return this.mongooseInstance;
+    }
 }
 DataAccess.connect();
-export { DataAccess };
+export {DataAccess};
