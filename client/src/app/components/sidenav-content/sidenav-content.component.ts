9ad16d1f51b3082ee0719b61cfe2cbd95e68e132
J.H <59863142+JoenHo@users.noreply.github.com>
2023-05-31 16:22:49 -0700

    Use dialog component in sidenav-content component

--- client/src/app/components/sidenav-content/sidenav-content.component.ts
+++ client/src/app/components/sidenav-content/sidenav-content.component.ts
@@ -1,8 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { UserService } from '../../services/user.service';
 import { RoomService } from '../../services/room.service';
 import { NoteService } from '../../services/note.service';
 import { ElementService } from '../../services/element.service';
+import { DialogComponent } from '../dialog/dialog.component';
 import { config } from 'src/app/config';
 
 @Component({
@@ -26,13 +28,24 @@ export class SidenavContentComponent implements OnInit{
     private userService: UserService,
     private roomService: RoomService,
     private noteService: NoteService,
-    private elementService: ElementService
+    private elementService: ElementService,
+    private dialog: MatDialog
   ) {}
 
   /** OnInit hook */
   ngOnInit() {
+    this.fetchUserData();
+  }
+
+  // fetch user data
+  fetchUserData() {
     this.userService.getUser().subscribe((data: any) => {
       this.user = data;
+
+      // clear lists
+      this.rooms = [];
+      this.elements = [];
+      this.notes = [];
 
       // fetch room data
       this.fetchRoomData();
@@ -81,6 +94,22 @@ export class SidenavContentComponent implements OnInit{
   onNoteClick(note: any) {
     this.noteService.setActiveNote(note);
     this.active_note = note.noteId;
+
+    // open dialog
+    const dialogRef = this.dialog.open(DialogComponent, {
+      data: {noteData: note, elements: this.elements},
+      width: '80vw',
+      height: '80vh'
+    });
+
+    // logic after dialog is closed
+    dialogRef.afterClosed().subscribe((refreshRequired) => {
+      this.active_note = '';
+      if(refreshRequired == true){
+        this.userService.setUser(this.user.userId);
+        this.fetchUserData();      
+      }
+    });  
   }
 
 }
