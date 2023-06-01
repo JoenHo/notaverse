b00d6397b3fc3beb17417598a41db4113df615b9
J.H <59863142+JoenHo@users.noreply.github.com>
2023-05-31 21:04:54 -0700

    Update port number to 8080

--- client/src/app/config.ts
+++ client/src/app/config.ts
@@ -1,4 +1,6 @@
 export const config = {
-    SERVER_URL: 'http://localhost:3000',
-    ASSETS_URL: 'http://localhost:3000/assets/'
+    // SERVER_URL: 'http://localhost:3000',
+    // ASSETS_URL: 'http://localhost:3000/assets/'
+    SERVER_URL: 'http://localhost:8080',
+    ASSETS_URL: 'http://localhost:8080/assets/'
 };
--- server/src/config.ts
+++ server/src/config.ts
@@ -1,4 +1,5 @@
-const SERVER_PORT = 3000;
+// const SERVER_PORT = 3000;
+const SERVER_PORT = 8080;
 const MONGO_URL = 'mongodb://127.0.0.1:27017/notaverse';
 const ATLAS_URL = 'mongodb+srv://notaverse_admin:5SORp7DVWRt9VDZv@cluster0.wxbgpj2.mongodb.net/?retryWrites=true&w=majority';
 const GOOGLE_CLIENT_ID = '241156039018-672vr2ogqejvlcb2um2em0md49uhh6fm.apps.googleusercontent.com';
