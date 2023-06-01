280b5413d41557f55b405f7fc4410d8258d10cd6
Weizheng Zhang <vvqqdd@gmail.com>
2023-05-31 23:26:13 -0700

    Updated db_pop.js to populate data into Atalas

--- server/src/db_pop/db_pop.js
+++ server/src/db_pop/db_pop.js
@@ -1,36 +1,63 @@
+const { MongoClient } = require('mongodb');
 const fs = require('fs');
 
-db = db.getSiblingDB('notaverse');
+const uri =
+  'mongodb+srv://notaverse_admin:5SORp7DVWRt9VDZv@cluster0.wxbgpj2.mongodb.net/?retryWrites=true&w=majority';
+const client = new MongoClient(uri);
 
-// drop current collections
-db.users.drop();
-db.notes.drop();
-db.rooms.drop();
-db.room_items.drop();
-db.elements.drop();
+async function populateData() {
+  try {
+    await client.connect();
 
-// create user, notes collection
-db.createCollection("users")
-db.createCollection("notes")
-db.createCollection("rooms")
-db.createCollection("room_items")
-db.createCollection("elements")
+    const db = client.db('test');
 
-// get userCollection, noteCollection
-userCollection = db.getCollection("users");
-noteCollection = db.getCollection("notes");
-roomCollection = db.getCollection("rooms");
-roomItemCollection = db.getCollection("room_items");
-elementCollection = db.getCollection("elements");
+    // drop current collections
+    await db.collection('users').drop();
+    await db.collection('notes').drop();
+    await db.collection('rooms').drop();
+    await db.collection('room_items').drop();
+    await db.collection('elements').drop();
 
-// JSON data
-userData = JSON.parse(fs.readFileSync(__dirname + '/collection_data/users.json'));
-noteData = JSON.parse(fs.readFileSync(__dirname + '/collection_data/notes.json'));
-roomData = JSON.parse(fs.readFileSync(__dirname + '/collection_data/rooms.json'));
-elementData = JSON.parse(fs.readFileSync(__dirname + '/collection_data/elements.json'));
+    // create collections
+    await db.createCollection('users');
+    await db.createCollection('notes');
+    await db.createCollection('rooms');
+    await db.createCollection('room_items');
+    await db.createCollection('elements');
 
-// insert data to userCollection
-userCollection.insertMany(userData);
-noteCollection.insertMany(noteData);
-roomCollection.insertMany(roomData);
-elementCollection.insertMany(elementData);
+    // get collection references
+    const userCollection = db.collection('users');
+    const noteCollection = db.collection('notes');
+    const roomCollection = db.collection('rooms');
+    const roomItemCollection = db.collection('room_items');
+    const elementCollection = db.collection('elements');
+
+    // JSON data
+    const userData = JSON.parse(
+      fs.readFileSync(__dirname + '/collection_data/users.json')
+    );
+    const noteData = JSON.parse(
+      fs.readFileSync(__dirname + '/collection_data/notes.json')
+    );
+    const roomData = JSON.parse(
+      fs.readFileSync(__dirname + '/collection_data/rooms.json')
+    );
+    const elementData = JSON.parse(
+      fs.readFileSync(__dirname + '/collection_data/elements.json')
+    );
+
+    // insert data
+    await userCollection.insertMany(userData);
+    await noteCollection.insertMany(noteData);
+    await roomCollection.insertMany(roomData);
+    await elementCollection.insertMany(elementData);
+
+    console.log('Data populated successfully');
+  } catch (error) {
+    console.error('Error populating data:', error);
+  } finally {
+    await client.close();
+  }
+}
+
+populateData();
