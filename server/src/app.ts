9772bc3d7e771e1ec90bf2119755bccd7d22a3e4
J.H <59863142+JoenHo@users.noreply.github.com>
2023-06-03 17:42:11 -0700

    Use the correct path for angularDist static route

--- server/src/app.ts
+++ server/src/app.ts
@@ -56,7 +56,7 @@ class App {
 
         /** Set static routes */
         router.use('/assets', express.static(__dirname + '/assets/element_imgs'));
-        router.use('/', express.static(__dirname+'/angularDist'));
+        router.use('', express.static(__dirname+'/angularDist'));
 
         /** Health check */
         router.get('/ping', (req, res, next) => {
