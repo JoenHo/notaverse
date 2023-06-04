f3e927af4b4b592347329d34eafe2d26fe7aeff3
J.H <59863142+JoenHo@users.noreply.github.com>
2023-06-03 17:39:57 -0700

    Update google auth callback function

--- server/src/routes/AuthRoutes.ts
+++ server/src/routes/AuthRoutes.ts
@@ -1,15 +1,25 @@
-import express, { NextFunction, Request, Response} from 'express';
+import express from 'express';
 import passport from 'passport';
 
 /** Authenticate with Google */
 const googleAuth = passport.authenticate('google', {scope: ['profile']});
 
 /** Google callback URL */
-const googleAuthCallback = passport.authenticate('google', { failureRedirect: '/' }, (req: Request, res: Response) => {
-        console.log("successfully authenticated user and returned to callback page.");
-        let user_id = '42ab8ffd-2367-44fd-9568-87a19134053a'    // TODO
-        res.redirect('/#/home/' + user_id);
-    });
+const googleAuthCallback = (req: any, res: any, next: any) => {
+    passport.authenticate('google', { failureRedirect: '/' }, (err, user) => {
+      if (err) {
+        return res.redirect('/');
+      }
+      if (!user) {
+        return res.redirect('/');
+      }  
+      console.log("successfully authenticated user and returned to callback page.");
+      console.log('user:', user);
+      
+      let user_id = '42ab8ffd-2367-44fd-9568-87a19134053a'; // TODO: Replace with actual user ID
+      res.redirect('/#/home/' + user_id);
+    })(req, res, next);
+};
 
 /** Define routes */
 const router = express.Router();
