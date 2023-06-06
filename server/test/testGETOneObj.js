175c65672e2338f858ce399a77edf3f37de1773a
J.H <59863142+JoenHo@users.noreply.github.com>
2023-06-06 13:18:11 -0700

    Update test for getting one obj

--- server/test/testGETOneObj.js
+++ server/test/testGETOneObj.js
@@ -17,7 +17,7 @@ describe('Test HTTP GET single object', function(){
 		 
     before(function (done) {
         chai.request(address)
-			.get("/user/?userId=46cd2fed-5792-38je-1864-28o85734203c")
+			.get("/user/46cd2fed-5792-38je-1864-28o85734203c")
 			.end(function (err, res) {
 				requestResult = res.body;
 				response = res;
@@ -29,27 +29,27 @@ describe('Test HTTP GET single object', function(){
     
     it('Should return a user object', function (){
         expect(response).to.have.status(200);
-        expect(response.body).to.have.length.lessThanOrEqual(1);
+        expect(response.body).is.not.null;
         expect(response).to.have.headers;
     });
 
     it('The user object has known properties', function(){
-	    expect(requestResult[0]).to.include.keys('userId');
-        expect(response.body[0]).to.have.property('plan');
-        expect(response.body[0]).to.have.property('roomIdList');
-        expect(response.body[0]).to.have.property('noteIdList');
+	    expect(requestResult).to.include.keys('userId');
+        expect(response.body).to.have.property('plan');
+        expect(response.body).to.have.property('roomIdList');
+        expect(response.body).to.have.property('noteIdList');
 		expect(response.body).to.not.be.a.string;
 	});
 
     it('The user object has known properties with the correct types', function() {
-        expect(requestResult[0]).to.have.property('userId').that.is.a('string');
-        expect(response.body[0]).to.have.property('plan').that.is.a('string');
-        expect(response.body[0]).to.have.property('roomIdList').that.is.an('array');
-        response.body[0].roomIdList.forEach((roomId) => {
+        expect(requestResult).to.have.property('userId').that.is.a('string');
+        expect(response.body).to.have.property('plan').that.is.a('string');
+        expect(response.body).to.have.property('roomIdList').that.is.an('array');
+        response.body.roomIdList.forEach((roomId) => {
             expect(roomId).to.be.a('string');
         });
-        expect(response.body[0]).to.have.property('noteIdList').that.is.an('array');
-        response.body[0].noteIdList.forEach((noteId) => {
+        expect(response.body).to.have.property('noteIdList').that.is.an('array');
+        response.body.noteIdList.forEach((noteId) => {
             expect(noteId).to.be.a('string');
         });
     });
